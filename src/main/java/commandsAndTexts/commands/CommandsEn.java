133bacdb9323e145a46136209a32533e6e32b6cd
Dmitry.potapov <dmitry.potapov@btsdigital.kz>
2019-09-04 12:03:21 +0600

    поправил команды. Так как телега принимает команды только в нижнем регистре

--- src/main/java/commandsAndTexts/commands/CommandsEn.java
+++ src/main/java/commandsAndTexts/commands/CommandsEn.java
@@ -9,7 +9,7 @@ public enum CommandsEn {
             "contact with him and ask few questions. If new member will answer it right - bot will give him send message rights." +
             " Also silent members who not wrote any messages in short period after joining to chat should be removed by bot." +
             " \n\n >>>>>>>>> ATTENTION: you must give to bot all admin rights"),
-    defaultLanguageAdm("Language for all users by default. Admin settings. Example usage: /defaultLanguageAdmEn /defaultLanguageAdmRu. Supported: En, Ru"),
+    defaultlanguageadm("Language for all users by default. Admin settings. Example usage: /defaultlanguageadmen /defaultlanguageadmru. Supported: En, Ru"),
     //autocheckLanguageForAll("This option will make bot to recognize user's language from update if it no defined by user. But if it NULL will used GLOBAL language setting for user."),
     ;
 
